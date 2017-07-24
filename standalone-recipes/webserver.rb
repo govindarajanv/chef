1f4c6a9c8f1ab208cc3175fb98e27762d22b6968
govindarajanv <govindarajanv@gmail.com>
2017-07-24 07:45:11 +0530

    changes done for moving files into directories

---
+++ standalone-recipes/webserver.rb
@@ -0,0 +1,15 @@
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file 'var/www/html/index.html' do
+  content '<html>
+		<body>
+		<h1> Hello World <h1>
+		</body>
+	   </html>'
+end
+
+
