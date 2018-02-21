b5f018dd738232dcda9e0c1d7d95787a2bc8e7fb
root <govindarajanv@gmail.com>
2018-02-21 14:47:26 +0000

    Added code for logstash configurations

--- cookbooks/elk_stack/recipes/log_stash.rb
+++ cookbooks/elk_stack/recipes/log_stash.rb
@@ -14,3 +14,10 @@ end
 #firewalld_port '9200/tcp' do
 #	action :add
 #end
+#/etc/ssl
+#openssl req -x509 -days 365 -batch -nodes -newkey rsa:2048 -keyout logstash-forwarder.key -out logstash_frwrd.crt
+#/etc/logstash/conf.d/logstash.conf
+#firewalld_port '5044/tcp' do
+#	action :add
+#end
+
