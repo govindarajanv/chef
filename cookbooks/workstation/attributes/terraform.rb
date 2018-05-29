027f432e6b7ab44e5fda0acbe8095fdf759113ac
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-29 07:41:27 +0530

    sololearn ruby exercises

---
+++ cookbooks/workstation/attributes/terraform.rb
@@ -0,0 +1,2 @@
+node.default['terraform']['download_url']="https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip"
+node.default['terraform']['install_directory']="/usr/local/bin"
---
+++ cookbooks/workstation/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: workstation
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'unzip'
+package 'wget'
+
+
